70aad5640991eb676b6f2fcbe7fbe0966e109aac
Daniil Shumko <daniil.shumko@idt.net>
2021-01-26 17:42:18 +0000

    Fixing some exports

--- lib/strophe.dart
+++ lib/strophe.dart
@@ -2,4 +2,7 @@ library strophe;
 
 export 'src/bosh/bosh.dart';
 export 'src/core/core.dart';
+export 'src/core/Strophe.Builder.dart';
+export 'src/core/Strophe.Connection.dart';
 export 'src/websocket.dart';
+export 'src/plugins/plugins.dart';
